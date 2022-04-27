145526f26b6cc7f3b697510032805db2d1400887
Andreess237 <andresrue222@hotmail.com>
2022-04-27 14:43:03 -0500

    Proyecto final

--- McMovie/McMovie/Migrations/20220427192832_Rating.Designer.cs
+++ McMovie/McMovie/Migrations/20220427192832_Rating.Designer.cs
@@ -4,6 +4,7 @@ using McMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace McMovie.Migrations
 {
     [DbContext(typeof(McMovieContext))]
-    partial class McMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20220427192832_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
